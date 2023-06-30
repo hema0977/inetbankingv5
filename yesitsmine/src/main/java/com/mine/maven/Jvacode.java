e4f88e8ddcbb0c7a80756a79bea0235397045aae
hema0977 <Hema09677@gmail.com>
2023-06-30 10:36:41 +0100

    My 2nd commit.

---
+++ yesitsmine/src/main/java/com/mine/maven/Jvacode.java
@@ -0,0 +1,13 @@
+package com.mine.maven;
+
+public class Jvacode {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
